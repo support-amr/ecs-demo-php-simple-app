52ef77d297666205e212d8b1d907a9a43232aac5
nimishshrestha <shrestha.nimish@gmail.com>
2021-04-09 11:41:35 +1000

    text change again.

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>AMR - ECS Fargate Hello Project</h1>
-                <h2>Congratulations</h2>
+                <h2>Congratulations. Another change.</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php

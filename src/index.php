33cd970b557f4cec7410acc020d7422e021a5c72
nimishshrestha <shrestha.nimish@gmail.com>
2021-04-20 13:26:43 +1000

    switched docker to phpapache

--- src/index.php
+++ src/index.php
@@ -15,8 +15,8 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations. Third Change..</h2>
+                <h1>Simple PHP:7.3-apache</h1>
+                <h2>Congratulations.</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php

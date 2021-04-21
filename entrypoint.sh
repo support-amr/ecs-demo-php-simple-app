a165c55518af81c436571080b738b85ac506d7c2
nimishshrestha <shrestha.nimish@gmail.com>
2021-04-21 15:53:29 +1000

    Load project environment secrets from AWS::SSM

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
+## ------------------------
+## Setup Laravel for first time
+## ------------------------
+#cp /var/www/html/.env.example /var/www/html/.env
+#php /var/www/html/artisan key:generate --ansi
 
-cp /var/www/html/.env.example /var/www/html/.env
-php /var/www/html/artisan key:generate --ansi
+## ------------------------
+## PREPARE APP ENVIRONMENT
+## ------------------------
+# Prepare .env file from passed environment variable
+echo -e ${env_dump_without_spaces//;/\\n} > .env
+php artisan config:cache
+php artisan cache:clear
 
+## ------------------------
+## Your script
+## ------------------------
+
+
+## ------------------------
+## START NGINX
+## ------------------------
 php-fpm &
 #crond -L /proc/1/fd/1 -b -l 8 -c /etc/cron.d &
 nginx -g "daemon off;"

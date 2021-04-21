4362ccb1088355301a98bdee6dd6de918a29646f
nimishshrestha <shrestha.nimish@gmail.com>
2021-04-21 12:53:14 +1000

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+cp /var/www/html/.env.example /var/www/html/.env
+php /var/www/html/artisan key:generate --ansi
+
 php-fpm &
 #crond -L /proc/1/fd/1 -b -l 8 -c /etc/cron.d &
 nginx -g "daemon off;"
